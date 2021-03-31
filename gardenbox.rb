0e7fe2a157b5e1046694f7d99a62279dac1baf74
rcon <reg710@gmail.com>
2021-03-31 11:50:30 -0400

    Comment out copied code

--- gardenbox.rb
+++ gardenbox.rb
@@ -1,6 +1,9 @@
-require 'sqlite3' # specifies sqlite version to use.
-gardenboxesDB = SQLite3::Database.new("gardenbox.sqlite") # points to database file in same folder as program.
-gardenboxesDB.results_as_hash = true
-#returns all data from the database as a hash
-# veggies = gardenboxesDB.execute("SELECT rowid, * FROM Vegetables")
+# require 'sqlite3' # specifies sqlite version to use.
+# gardenboxesDB = SQLite3::Database.new("gardenbox.sqlite") # points to database file in same folder as program.
+# gardenboxesDB.results_as_hash = true
+# #returns all data from the database as a hash
+# # veggies = gardenboxesDB.execute("SELECT rowid, * FROM Vegetables")
+# taken from Bob's code
+
+
 
